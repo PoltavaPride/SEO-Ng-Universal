f0fbfd28b526ea462ca0ea5c1f285893046b57ca
Arsenii Tverdokhleb <poltavapride@gmail.com>
2018-08-02 21:44:12 +0300

    add base of ui module

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '../../../node_modules/@angular/router';
+import { FooterComponent } from './containers/footer/footer.component';
+import { HeaderComponent } from './containers/header/header.component';
+import { LayoutComponent } from './containers/layout/layout.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  declarations: [LayoutComponent, HeaderComponent, FooterComponent]
+})
+export class UiModule { }
